465a34cf370ab9de772769e225aaaf246dd84305
Admin <Admin@DELMARCO>
2017-08-14 13:29:19 +0300

    Add project files.

---
+++ lab6/ComplexNum.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    public struct ComplexNum
+    {
+        public double Real { get; set; }
+        public double Img { get; set; }
+
+        public ComplexNum (double r, double i)
+        {
+            Real = r;
+            Img = i;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj.GetType() != typeof(ComplexNum))
+                return false;
+            return (ComplexNum)obj == this;
+        }
+        public override string ToString()
+        {
+            return string.Format("real = {0}, imaginary = {1}", this.Real, this.Img);
+        }
+        public override int GetHashCode()
+        {
+            int result = 17;
+            result = 37 * result + (int)(BitConverter.DoubleToInt64Bits(Real) - (BitConverter.DoubleToInt64Bits(Real) >> 32));
+            result = 37 * result + (int)(BitConverter.DoubleToInt64Bits(Img) - (BitConverter.DoubleToInt64Bits(Img) >> 32));
+            return result;
+        }
+        public static ComplexNum operator+ (ComplexNum num1, ComplexNum num2)
+        {
+            return new ComplexNum(num1.Real + num2.Real, num1.Img + num2.Img);
+        }
+        public static ComplexNum operator -(ComplexNum num1, ComplexNum num2)
+        {
+            return new ComplexNum(num1.Real - num2.Real, num1.Img - num2.Img);
+        }
+        public static ComplexNum operator *(ComplexNum num1, ComplexNum num2)
+        {
+            return new ComplexNum(num1.Real*num2.Real - num1.Img*num2.Img, num1.Img*num2.Real + num2.Img*num1.Real);
+        }
+        public static ComplexNum operator /(ComplexNum num1, ComplexNum num2)
+        {
+            double div = Math.Pow(num2.Real, 2) + Math.Pow(num2.Img, 2);
+            return new ComplexNum((num1.Real * num2.Real + num1.Img * num2.Img)/div, (num1.Img * num2.Real - num2.Img * num1.Real)/div);
+        }
+
+        public static bool operator ==(ComplexNum num1, ComplexNum num2)
+        {
+            return num1.Real == num2.Real && num1.Img==num2.Img;
+        }
+        public static bool operator !=(ComplexNum num1, ComplexNum num2)
+        {
+            return !(num1==num2);
+        }
+
+
+    }
+}
---
+++ lab6/TimeInterval.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    public struct TimeInterval
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeInterval (DateTime start, DateTime end)
+        {
+            if (!CheckInterval(start, end))
+                throw new Exception("Invalid time interval");
+            Start = start;
+            End = end;
+        }
+        private static bool CheckInterval(DateTime start, DateTime end)
+        {
+            return start<end;
+        }
+        public TimeSpan GetTimeSpan()
+        {
+            return End - Start;
+        }
+        public override string ToString()
+        {
+            return string.Format("Start of event is {0}, end of event is {1}", Start, End);
+        }
+        public static bool operator ==(TimeInterval first, TimeInterval second)
+        {
+            return first.Start == second.Start && first.End == second.End;
+        }
+        public static bool operator !=(TimeInterval first, TimeInterval second)
+        {
+            return first.Start != second.Start || first.End != second.End;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj ==null)
+                return false;
+            if (obj.GetType() != typeof (TimeInterval))
+                return false;
+            return (TimeInterval)obj == this;
+        }
+        public override int GetHashCode()
+        {
+            int result = 17;
+            result = 37 * result + (int)(Start.ToBinary() - (Start.ToBinary() >> 32));
+            result = 37 * result + (int)(End.ToBinary() - (End.ToBinary()>> 32));
+            return result;
+            
+        }
+    }
+}
